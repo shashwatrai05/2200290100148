d16299d7b842ecd3144d47b1ac5ba7ecc7720aae
Shashwat Rai <shashwatrai575@gmail.com>
2025-05-09 18:51:19 +0530

    Average Calculator Project Setup

---
+++ src/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 9876,
+  WINDOW_SIZE: parseInt(process.env.WINDOW_SIZE) || 10,
+  THIRD_PARTY_BASE_URL: process.env.THIRD_PARTY_BASE_URL,
+};
